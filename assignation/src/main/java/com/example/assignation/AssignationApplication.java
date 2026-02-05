5a48eb7d3647a4cbcc11fd00446d1eb3fbac10d4
eliaTolojanahary <tolojanaharyelia845@gmail.com>
2026-02-05 09:06:49 +0100

    initiation

---
+++ assignation/src/main/java/com/example/assignation/AssignationApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignationApplication.class, args);
+	}
+
+}
---
+++ assignation/src/test/java/com/example/assignation/AssignationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
